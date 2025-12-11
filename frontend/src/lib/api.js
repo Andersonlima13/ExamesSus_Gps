dc60faadbdbb43498abce0879deaf70a0aa8590e
ruby <anderson.lima18pb@gmail.com>
2025-12-11 17:48:48 -0300

    problema com axios resolvido

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
+import xhrAdapter from "axios/lib/adapters/xhr.js";
 
 const api = axios.create({
   baseURL: "https://examessus-gps.onrender.com",
-  headers: {
-    "Content-Type": "application/json"
-  }
+  headers: { "Content-Type": "application/json" },
+  adapter: xhrAdapter
 });
 
 export default api;
